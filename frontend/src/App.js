c641b1ceb1ccc0fb662ecba9cb71016c9bb81fd5
Sakshi Arjun <sakshiarjun@MacBook-Pro-2.local>
2026-04-25 13:04:07 +0530

    Analytics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const [instName, setInstName] = useState("");
@@ -432,6 +433,7 @@ function App() {
           </>
         )}
       </div>
+      <Analytics />
     </div>
   );
 }
